c38b18c0d31430e2d1ed272b360d273dbd9c779a
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-04-09 21:53:02 -0500

    primera prueba
    agregue la imagen y sale un texto que saluda al usuario

--- Practica3/Interaccion con imagenes/app/src/main/java/com/example/interaccionconimagenes/MainActivity.kt
+++ Practica3/Interaccion con imagenes/app/src/main/java/com/example/interaccionconimagenes/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.interaccionconimagenes
 
+import android.media.Image
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
+import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val boton_imagen = findViewById<ImageButton>(R.id.btn_imagen)
+        boton_imagen.setOnClickListener(){
+            val mensaje = "hola usuario"
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
         }
+
     }
 }
