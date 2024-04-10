6e237c8a987b7301b2ecf5d4e18c989694b897cd
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-04-09 22:04:43 -0500

    mensaje
    se logro agregar el mensaje y sale en el telefono tocando la imagen

--- Practica3/Interaccion con imagenes/app/src/main/java/com/example/interaccionconimagenes/MainActivity.kt
+++ Practica3/Interaccion con imagenes/app/src/main/java/com/example/interaccionconimagenes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.interaccionconimagenes
 
 import android.media.Image
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 
@@ -13,11 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val texto_personalizado = findViewById<EditText>(R.id.txt_personalizado)
+
         val boton_imagen = findViewById<ImageButton>(R.id.btn_imagen)
         boton_imagen.setOnClickListener(){
-            val mensaje = "hola usuario"
+            val mensaje = texto_personalizado.text
             Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
         }
+
 
     }
 }
