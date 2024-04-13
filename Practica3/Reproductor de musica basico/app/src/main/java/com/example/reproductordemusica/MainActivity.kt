b923e3d8554686f1894328b3283ee67ce1249635
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-04-12 22:23:14 -0500

    pausa y reproducir
    se agrego la interfaz y se logro colocar las funciones para que se pause y se reproduzca la musica

--- Practica3/Reproductor de musica basico/app/src/main/java/com/example/reproductordemusica/MainActivity.kt
+++ Practica3/Reproductor de musica basico/app/src/main/java/com/example/reproductordemusica/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.reproductordemusica
 
+import android.media.MediaPlayer
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var musica = MediaPlayer.create(this, R.raw.pista1)
+        val btn_reproducir = findViewById<Button>(R.id.btn_reproducir)
+        btn_reproducir.setOnClickListener(){
+            musica.start()
+        }
+
+        val btn_pausar = findViewById<Button>(R.id.btn_pausar)
+        btn_pausar.setOnClickListener(){
+            musica.pause()
+        }
+
+
     }
+
 }
