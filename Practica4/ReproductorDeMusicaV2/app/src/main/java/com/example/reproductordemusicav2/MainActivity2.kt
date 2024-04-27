490316028675d85afbb7653cdfea6243d741897f
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-04-27 00:52:26 -0500

    Agregue el nombre de la musica, trabajo terminado

--- Practica4/ReproductorDeMusicaV2/app/src/main/java/com/example/reproductordemusicav2/MainActivity2.kt
+++ Practica4/ReproductorDeMusicaV2/app/src/main/java/com/example/reproductordemusicav2/MainActivity2.kt
@@ -4,6 +4,7 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +13,24 @@ class MainActivity2 : AppCompatActivity() {
         lateinit var musica : MediaPlayer
         val seleccionado = intent.getIntExtra(KEY_SELECT, 0)
 
-
+        val txt_musica = findViewById<TextView>(R.id.txt_musica)
         when(seleccionado){
-            0 -> musica = MediaPlayer.create(this, R.raw.pista1)
-            1 -> musica = MediaPlayer.create(this, R.raw.pista2)
-            2 -> musica = MediaPlayer.create(this, R.raw.pista3)
-            3 -> musica = MediaPlayer.create(this, R.raw.pista4)
-            4 -> musica = MediaPlayer.create(this, R.raw.pista5)
+
+            0 -> {musica = MediaPlayer.create(this, R.raw.pista1)
+            txt_musica.text = "Emperadors new clothes"
+            }
+            1 -> {musica = MediaPlayer.create(this, R.raw.pista2)
+                txt_musica.text = "Miss Jackson"
+            }
+            2 -> {musica = MediaPlayer.create(this, R.raw.pista3)
+                txt_musica.text = "bettersweet"
+            }
+            3 -> {musica = MediaPlayer.create(this, R.raw.pista4)
+                txt_musica.text = "One of the drunks"
+            }
+            4 -> {musica = MediaPlayer.create(this, R.raw.pista5)
+                txt_musica.text = "Death of the barchelor"
+            }
         }
 
 
@@ -39,6 +51,7 @@ class MainActivity2 : AppCompatActivity() {
                 musica.prepare()
             }
         }
+
 
 
 
