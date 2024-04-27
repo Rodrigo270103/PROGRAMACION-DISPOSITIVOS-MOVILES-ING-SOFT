d167fe4115c976545b7529bfe08c98c3fa0df8e0
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-04-27 01:12:25 -0500

    Se crearon 2 fragments

---
+++ Practica5/InteraccionConImagenesV3/app/src/main/java/com/example/interaccionconimagenesv3/InteraccionFragment.kt
@@ -0,0 +1,17 @@
+package com.example.interaccionconimagenesv3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class InteraccionFragment : Fragment(R.layout.fragment_interaccion) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ Practica5/InteraccionConImagenesV3/app/src/main/java/com/example/interaccionconimagenesv3/MenuFragments.kt
@@ -0,0 +1,18 @@
+package com.example.interaccionconimagenesv3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MenuFragments : Fragment(R.layout.fragment_menu_fragments) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
