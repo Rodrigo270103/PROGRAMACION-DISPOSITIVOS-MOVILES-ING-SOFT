056a925e5a012a251fd236f1af819a5c3292db2c
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2024-04-26 22:48:44 -0500

    se creo la funciom de spinner y se hizo el salto de actividad

--- Practica4/app/src/main/java/com/example/interaccionconimagenesv2/MainActivity.kt
+++ Practica4/app/src/main/java/com/example/interaccionconimagenesv2/MainActivity.kt
@@ -1,27 +1,41 @@
 package com.example.interaccionconimagenesv2
 
+import android.content.Intent
 import android.media.Image
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Spinner
 import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
 
+const val KEY_SELECT = "clave"
+const val REQUEST_CODE = 123
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val texto_personalizado = findViewById<EditText>(R.id.txt_personalizado)
+        val spinner = findViewById<Spinner>(R.id.spn_opciones)
+        val opciones = listOf("imagen1", "imagen2", "imagen3", "imagen4")
+        val adapter = ArrayAdapter(this, com.google.android.material.R.layout.mtrl_auto_complete_simple_item, opciones)
+        spinner.adapter = adapter
 
-        val boton_imagen = findViewById<ImageButton>(R.id.btn_imagen)
-        boton_imagen.setOnClickListener(){
-            val mensaje = texto_personalizado.text
-            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+
+
+        val button = findViewById<Button>(R.id.btn_siguiente)
+        button.setOnClickListener(){
+            val seleccionado = spinner.selectedItemPosition //preparando la informacion para el casmbio de actividad
+
+            val intent = Intent(this, MainActivity2::class.java) // para sdaber a donde estoy yendo
+            intent.putExtra(KEY_SELECT, seleccionado)
+
+            startActivityForResult(intent, REQUEST_CODE)
         }
-
 
     }
 }
